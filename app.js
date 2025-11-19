a1b3f19d05d8be3901c4ad4042e9fc69d08384fc
leedongkyu <leedongkyu0701@gmail.com>
2025-11-19 16:31:40 +0900

    프론트엔드 url 수정

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(compress());
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://test-frontend-five-pearl.vercel.app",
   credentials: true,
 }));
 app.use(cookieParser());
