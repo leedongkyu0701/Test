3de866dc34543bf6a1ca4c8d4538569bb8f35d60
leedongkyu <leedongkyu0701@gmail.com>
2025-11-19 16:38:28 +0900

    url 수정

--- socket.js
+++ socket.js
@@ -4,7 +4,7 @@ module.exports = {
     init: (server) => {
         io = require("socket.io")(server, {
             cors: {
-                origin: "http://localhost:5173",
+                origin: "https://test-frontend-five-pearl.vercel.app",
                 methods: ["GET", "POST"],
                 credentials: true,
             },
